66cfc4028011bced2f35f425e463b01741eff5f2
michelewu1 <michelewu1@gmail.com>
2015-09-08 18:57:08 -0400

    Pset 2 COmpleted

--- psets/2.js
+++ psets/2.js
@@ -12,3 +12,10 @@ Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no mo
 */
 
 // write your solution here...
+var CurrentAge = 17;
+var maxAge= 100;
+var amount= 2;
+
+var supply = (maxAge - CurrentAge)*(365*2);
+
+console.log("You will need " + supply + " to last you until the ripe old age of " + maxAge + ".");
